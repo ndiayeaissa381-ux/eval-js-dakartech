c5e24381f2f5a80dc62d833572d15794daf3e7a7
AissaNdiaye <ndiayeaissa381@gmail.com>
2026-03-10 11:43:56 +0100

    3e commit avec l'ajout de tache et affichage de la liste dans le DOM

---
+++ script.js
@@ -0,0 +1,36 @@
+let taches = [
+    { texte: "Faire les courses", terminee: false },
+    { texte: "Réviser JavaScript", terminee: false },
+    { texte: "Appeler maman", terminee: false }
+];
+
+let listeTaches = document.querySelector("#liste_taches");
+let messageErreur = document.querySelector("#message_erreur");
+let compteur = document.querySelector("#compteur");
+function afficherListe() {
+    listeTaches.innerHTML = "";
+
+    for (let i = 0; i < taches.length; i++) {
+        let tache = taches[i];
+
+        let li = document.createElement("li");
+        li.className = "tache";
+
+        if (tache.terminee === true) {
+            li.className = "tache terminee";
+        }
+
+        let span = document.createElement("span");
+        span.textContent = tache.texte;
+
+        let btnTerminer = document.createElement("button");
+        btnTerminer.className = "btn_terminer";
+        btnTerminer.textContent = "Terminé";
+
+        li.appendChild(span);
+        li.appendChild(btnTerminer);
+        listeTaches.appendChild(li);
+    }
+}
+
+afficherListe();
